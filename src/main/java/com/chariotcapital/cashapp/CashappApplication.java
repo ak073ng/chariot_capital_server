d2d2df26c2653eab4d2e2f5ffc9938c7db221b4a
Andrew Koteng <32411583+ak073ng@users.noreply.github.com>
2021-05-06 12:40:33 +0300

    config for heroku

--- src/main/java/com/chariotcapital/cashapp/CashappApplication.java
+++ src/main/java/com/chariotcapital/cashapp/CashappApplication.java
@@ -3,9 +3,19 @@ package com.chariotcapital.cashapp;
 import com.chariotcapital.cashapp.utility.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class CashappApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Chariot Capital server is running!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CashappApplication.class, args);
