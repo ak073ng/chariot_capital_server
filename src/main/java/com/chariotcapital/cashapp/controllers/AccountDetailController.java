9aad8718bbf21e57c79bc8b59ae042be0c685cea
Andrew Koteng <32411583+ak073ng@users.noreply.github.com>
2021-05-10 12:08:41 +0300

    updated account details controller

--- src/main/java/com/chariotcapital/cashapp/controllers/AccountDetailController.java
+++ src/main/java/com/chariotcapital/cashapp/controllers/AccountDetailController.java
@@ -1,10 +1,14 @@
 package com.chariotcapital.cashapp.controllers;
 
 import com.chariotcapital.cashapp.models.AccountDetail;
+import com.chariotcapital.cashapp.models.User;
 import com.chariotcapital.cashapp.repositories.AccountDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path="/api/acc_details")
@@ -12,48 +16,103 @@ public class AccountDetailController {
     @Autowired
     private AccountDetailRepository acc_detailRepository;
 
-    //add account details
-    @PostMapping(path="/add_account")
-    public AccountDetail addAccount (@RequestBody AccountDetail acc_detail) {
-        Object does_user_exist = acc_detailRepository.findByUserToken(acc_detail.getUserToken());
+    //add account number
+    @PostMapping(path="/add_account_number")
+    public Map<String, AccountDetail> addAccountNumber (@RequestBody Map<String, AccountDetail> map_request) {
+        Map<String, AccountDetail> map = new HashMap<>();
+
+        AccountDetail acc_detail = map_request.get("account");
+
+        AccountDetail does_user_exist = acc_detailRepository.findByUserToken(acc_detail.getUserToken());
 
         AccountDetail user_acc_detail = new AccountDetail();
 
         if(does_user_exist == null){
             user_acc_detail.setUserToken(acc_detail.getUserToken());
             user_acc_detail.setAccountNumber(acc_detail.getAccountNumber());
-            user_acc_detail.setDebit(acc_detail.getDebit());
-            user_acc_detail.setCredit(acc_detail.getCredit());
-            user_acc_detail.setAccountBalance(acc_detail.getAccountBalance());
 
             acc_detailRepository.save(user_acc_detail);
+
+            map.put("account", user_acc_detail);
+
+            return map;
         }
 
-        return user_acc_detail;
+        user_acc_detail.setUserToken(null);
+        user_acc_detail.setAccountNumber(null);
+
+        map.put("account", user_acc_detail);
+
+        return map;
     }
 
-    //update existing account
-    @PutMapping(path="/update_account")
-    public AccountDetail updateAccount (@RequestBody AccountDetail acc_detail) {
-        AccountDetail user_acc_detail = acc_detailRepository.findByUserToken(acc_detail.getUserToken());
+    //update existing account number
+    @PatchMapping(path="/update_account_number")
+    public Map<String, AccountDetail> updateAccountNumber (@RequestBody Map<String, AccountDetail> map_request) {
+        Map<String, AccountDetail> map = new HashMap<>();
 
-        if(user_acc_detail != null){
-            user_acc_detail.setUserToken(acc_detail.getUserToken());
+        AccountDetail acc_detail = map_request.get("account");
+
+        AccountDetail does_user_exist = acc_detailRepository.findByUserToken(acc_detail.getUserToken());
+
+        AccountDetail user_acc_detail = new AccountDetail();
+
+        if(does_user_exist != null){
             user_acc_detail.setAccountNumber(acc_detail.getAccountNumber());
-            user_acc_detail.setDebit(acc_detail.getDebit());
-            user_acc_detail.setCredit(acc_detail.getCredit());
-            user_acc_detail.setAccountBalance(acc_detail.getAccountBalance());
 
             acc_detailRepository.save(user_acc_detail);
+
+            map.put("account", user_acc_detail);
+
+            return map;
         }
 
-        return user_acc_detail;
+        user_acc_detail.setAccountNumber(null);
+
+        map.put("account", user_acc_detail);
+
+        return map;
+    }
+
+    //update existing account info
+    @PatchMapping(path="/update_account_info")
+    public Map<String, AccountDetail> updateAccountInfo (@RequestBody Map<String, AccountDetail> map_request) {
+        Map<String, AccountDetail> map = new HashMap<>();
+
+        AccountDetail acc_detail = map_request.get("account");
+
+        AccountDetail does_user_exist = acc_detailRepository.findByUserToken(acc_detail.getUserToken());
+
+        AccountDetail user_acc_detail = new AccountDetail();
+
+        if(does_user_exist != null){
+            user_acc_detail.setDebit(acc_detail.getDebit());
+            user_acc_detail.setCredit(acc_detail.getCredit());
+            user_acc_detail.setAccountBalance(acc_detail.getDebit() - acc_detail.getCredit());
+
+            acc_detailRepository.save(user_acc_detail);
+
+            map.put("account", user_acc_detail);
+
+            return map;
+        }
+
+        user_acc_detail.setDebit(0);
+        user_acc_detail.setCredit(0);
+        user_acc_detail.setAccountBalance(0);
+
+        map.put("account", user_acc_detail);
+
+        return map;
     }
 
     //get all accounts in system
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<AccountDetail> getAllAccounts() {
-        // This returns a JSON or XML with the users
-        return acc_detailRepository.findAll();
+    public @ResponseBody Map<String, Iterable<AccountDetail>> getAllAccounts() {
+        Map<String, Iterable<AccountDetail>> map = new HashMap<>();
+
+        map.put("accounts", acc_detailRepository.findAll());
+
+        return map;
     }
 }
