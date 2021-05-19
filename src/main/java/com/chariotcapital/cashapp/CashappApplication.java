ee6a6cf4466921d005c967639e043bf0a8de2244
Andrew Koteng <32411583+ak073ng@users.noreply.github.com>
2021-05-19 09:24:50 +0200

    added privacy policy

--- src/main/java/com/chariotcapital/cashapp/CashappApplication.java
+++ src/main/java/com/chariotcapital/cashapp/CashappApplication.java
@@ -3,17 +3,50 @@ package com.chariotcapital.cashapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 
 @Controller
 @SpringBootApplication
 public class CashappApplication {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	@ResponseBody
 	String home() {
-		return "Chariot Capital server is running! - test 12";
+		return "CC server is running.";
+	}
+
+	@GetMapping(value = "/privacy-policy")
+	public StreamingResponseBody getStreamingFile(HttpServletResponse response) throws URISyntaxException {
+
+		File file = new File(getClass().getClassLoader().getResource("templates/cc-privacy-policy.pdf").toURI());
+
+		//viewing in web browser
+		response.setContentType("application/pdf");
+		//for downloading the file directly if viewing is not possible
+		response.setHeader("Content-Disposition", "inline; filename=" + file.getName());
+
+		file = null;
+
+		//put the directory architecture according to your target directory
+		// generated during compilation in maven spring boot
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("templates/cc-privacy-policy.pdf");
+
+		return outputStream -> {
+			int nRead;
+			byte[] data = new byte[1024];
+			while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+				outputStream.write(data, 0, nRead);
+			}
+			inputStream.close();
+		};
 	}
 
 	public static void main(String[] args) {
